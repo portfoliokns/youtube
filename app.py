b87e1577714eaeaa5ded9968dec8a1a104ee917f
konishi333 <jobhuntingkonishi3@gmail.com>
2024-09-13 23:57:05 +0900

    console.log削除

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 # ここへ、観たいyoutube動画のURLを載せる（ショート動画やクリップ動画は不可）
 videoURLs = [
-  "https://www.youtube.com/watch?v=7XXq6zb74Ss&t=20s",
+  "https://www.youtube.com/watch?v=7XXq6zb74Ss",
   "https://www.youtube.com/watch?v=AJRPMmyAEAQ&t=16s",
-  # "https://www.youtube.com/watch?v=VIDEO_ID_1", "https://www.youtube.com/watch?v=VIDEO_ID_2", "https://www.youtube.com/watch?v=VIDEO_ID_3"
+  # "https://www.youtube.com/watch?v=x_fHq3B_UP4", #ライブカメラ映像
 ]
 
 @app.route('/')
