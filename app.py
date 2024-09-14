5c9477828697fe29cee85dc5a50119a94e2f2cd8
konishi333 <jobhuntingkonishi3@gmail.com>
2024-09-14 15:09:24 +0900

    共通テンプレート/部分テンプレート適応
    プレイリスト機能の基本的な動作に問題なし

--- app.py
+++ app.py
@@ -13,5 +13,9 @@ videoURLs = [
 def index():
   return render_template('index.html', videoURLs=videoURLs)
 
+@app.route('/playlist')
+def playlist():
+  return render_template('playlist.html', videoURLs=videoURLs)
+
 if __name__ == "__main__":
   app.run(debug=True)
